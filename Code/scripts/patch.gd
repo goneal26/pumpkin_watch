7c30ac7ece2a1669ec873802f7afa5dc36830dc0
goneal26 <g.oneal26@ncf.edu>
2023-10-16 14:06:15 -0400

    Light testing

---
+++ Code/scripts/patch.gd
@@ -0,0 +1,25 @@
+extends TileMap
+
+@export var pumpkin_spawn_weight: float = 0.1
+
+var map_size: Vector2i = Vector2i(36, 20)
+
+const VINE_ATLAS_COORD: Vector2i = Vector2i(1, 0)
+
+func spawn_pumpkins():
+	for x in map_size.x:
+		for y in map_size.y:
+			if get_cell_atlas_coords(0, Vector2i(x, y)) == VINE_ATLAS_COORD and randf() < pumpkin_spawn_weight:
+				var pumpkin = load("res://props/pumpkin.tscn")
+				var pumpkin_instance = pumpkin.instantiate()
+				get_parent().add_child.call_deferred(pumpkin_instance)
+				pumpkin_instance.position = map_to_local(Vector2i(x, y))
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	spawn_pumpkins()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
